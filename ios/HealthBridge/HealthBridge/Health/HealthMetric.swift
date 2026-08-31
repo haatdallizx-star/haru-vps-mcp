fb5fb61263fc1978fccaef551df8fe39646a4031
haatdallizx-star <haatdallizx@gmail.com>
2026-08-31 15:39:52 +0800

    refactor: expose HealthKit metric identifiers to app layer

--- ios/HealthBridge/HealthBridge/Health/HealthMetric.swift
+++ ios/HealthBridge/HealthBridge/Health/HealthMetric.swift
@@ -1,6 +1,6 @@
 import HealthKit
 
-enum HealthMetric: String, CaseIterable {
+public enum HealthMetric: String, CaseIterable {
     case heartRate = "heart_rate"
     case hrv = "hrv"
     case steps = "steps"
@@ -8,14 +8,10 @@ enum HealthMetric: String, CaseIterable {
 
     var sampleType: HKSampleType? {
         switch self {
-        case .heartRate:
-            return HKObjectType.quantityType(forIdentifier: .heartRate)
-        case .hrv:
-            return HKObjectType.quantityType(forIdentifier: .heartRateVariabilitySDNN)
-        case .steps:
-            return HKObjectType.quantityType(forIdentifier: .stepCount)
-        case .sleep:
-            return HKObjectType.categoryType(forIdentifier: .sleepAnalysis)
+        case .heartRate: return HKObjectType.quantityType(forIdentifier: .heartRate)
+        case .hrv: return HKObjectType.quantityType(forIdentifier: .heartRateVariabilitySDNN)
+        case .steps: return HKObjectType.quantityType(forIdentifier: .stepCount)
+        case .sleep: return HKObjectType.categoryType(forIdentifier: .sleepAnalysis)
         }
     }
 }
