e619aea23d04911018bae783bdc59d1576127873
haatdallizx-star <haatdallizx@gmail.com>
2026-08-31 15:49:03 +0800

    fix: type HealthKit authorization continuation

--- ios/HealthBridge/HealthBridge/Health/HealthKitManager.swift
+++ ios/HealthBridge/HealthBridge/Health/HealthKitManager.swift
@@ -4,24 +4,18 @@ import HealthKit
 public final class HealthKitManager {
     public let store: HKHealthStore
 
-    public init(store: HKHealthStore = HKHealthStore()) {
-        self.store = store
-    }
+    public init(store: HKHealthStore = HKHealthStore()) { self.store = store }
 
-    var readTypes: Set<HKObjectType> {
-        Set(HealthMetric.allCases.compactMap { $0.sampleType })
-    }
+    var readTypes: Set<HKObjectType> { Set(HealthMetric.allCases.compactMap { $0.sampleType }) }
 
-    public static func initialSyncStart(now: Date = Date()) -> Date {
-        now.addingTimeInterval(-24 * 60 * 60)
-    }
+    public static func initialSyncStart(now: Date = Date()) -> Date { now.addingTimeInterval(-24 * 60 * 60) }
 
     public static func backgroundFrequency(for metric: HealthMetric) -> HKUpdateFrequency {
         metric == .steps ? .hourly : .immediate
     }
 
     public func requestAuthorization() async throws {
-        try await withCheckedThrowingContinuation { continuation in
+        try await withCheckedThrowingContinuation { (continuation: CheckedContinuation<Void, Error>) in
             store.requestAuthorization(toShare: [], read: readTypes) { success, error in
                 if let error { continuation.resume(throwing: error) }
                 else if success { continuation.resume() }
@@ -30,17 +24,11 @@ public final class HealthKitManager {
         }
     }
 
-    func anchoredQuery(
-        metric: HealthMetric,
-        anchor: HKQueryAnchor?,
-        startDate: Date?,
-        handler: @escaping ([HKSample], [HKDeletedObject], HKQueryAnchor?, Error?) -> Void
-    ) -> HKAnchoredObjectQuery? {
+    func anchoredQuery(metric: HealthMetric, anchor: HKQueryAnchor?, startDate: Date?, handler: @escaping ([HKSample], [HKDeletedObject], HKQueryAnchor?, Error?) -> Void) -> HKAnchoredObjectQuery? {
         guard let type = metric.sampleType else { return nil }
         let predicate = startDate.map { HKQuery.predicateForSamples(withStart: $0, end: nil, options: []) }
         return HKAnchoredObjectQuery(type: type, predicate: predicate, anchor: anchor, limit: HKObjectQueryNoLimit) {
-            _, samples, deletions, newAnchor, error in
-            handler(samples ?? [], deletions ?? [], newAnchor, error)
+            _, samples, deletions, newAnchor, error in handler(samples ?? [], deletions ?? [], newAnchor, error)
         }
     }
 
@@ -60,15 +48,10 @@ public final class HealthKitManager {
     public func installObservers(onChange: @escaping (HealthMetric, @escaping () -> Void) -> Void) {
         for metric in HealthMetric.allCases {
             guard let type = metric.sampleType else { continue }
-            let query = HKObserverQuery(sampleType: type, predicate: nil) { _, completion, _ in
-                onChange(metric, completion)
-            }
+            let query = HKObserverQuery(sampleType: type, predicate: nil) { _, completion, _ in onChange(metric, completion) }
             store.execute(query)
         }
     }
 }
 
-public enum HealthKitManagerError: Error {
-    case authorizationNotGranted
-    case backgroundDeliveryNotEnabled
-}
+public enum HealthKitManagerError: Error { case authorizationNotGranted, backgroundDeliveryNotEnabled }
