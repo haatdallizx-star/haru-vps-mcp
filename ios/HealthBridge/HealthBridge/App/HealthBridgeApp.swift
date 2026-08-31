ab47752ca840b81a7a66633e1cff9f51fd7f5b5d
haatdallizx-star <haatdallizx@gmail.com>
2026-08-31 15:40:47 +0800

    feat: attach HealthBridge launch delegate

--- ios/HealthBridge/HealthBridge/App/HealthBridgeApp.swift
+++ ios/HealthBridge/HealthBridge/App/HealthBridgeApp.swift
@@ -2,9 +2,9 @@ import SwiftUI
 
 @main
 struct HealthBridgeApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) private var appDelegate
+
     var body: some Scene {
-        WindowGroup {
-            ContentView()
-        }
+        WindowGroup { ContentView() }
     }
 }
